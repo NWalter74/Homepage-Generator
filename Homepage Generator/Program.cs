77e253f50310985cc6f2c475a199f60cc53cb51a
NWalter74 <nicole.walter@iths.se>
2022-09-16 13:20:37 +0200

    Add project files.

---
+++ Homepage Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homepage_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
